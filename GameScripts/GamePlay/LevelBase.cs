fa437189e214ade7670febf88de57a03cb5d1dab
Zachary Barnett <extraontheside@hotmail.com>
2016-11-07 11:54:37 -0700

    11-7

--- GameScripts/GamePlay/LevelBase.cs
+++ GameScripts/GamePlay/LevelBase.cs
@@ -20,10 +20,31 @@ public class LevelBase {
             break;
 
             case 1:
-                Console.WriteLine();
+                Console.WriteLine("You've " + traveled + " into " + objects[i]);
+            break;
+
+            case 2:
+                Console.WriteLine("You've " + traveled + " into " + objects[i]);
+                Game.GameTimer();
+                Random randomNumber = new Random();
+                Game.UnderWater.Encounter(randomNumber.Next(0, Game.UnderWater.objects.LevelBase));
+            break;
+
+            case 3:
+            //Danger kills us and ends the Game
+                Console.WriteLine("You've " + traveled + " into " + objects[i]);
+                if(objects[i] == "Shark") {
+                    M16.SemiAutoFire();
+                }
+                Game.canPlay = false;
+            break;
+
             default:
+                Console.WriteLine("Your path is clear.");
+            break;
         }
-        
+    }
+}
         
         /*if(i < objects.Length) {
             Console.WriteLine("You've walked into " + objects[i]);
@@ -36,4 +57,8 @@ public class LevelBase {
         
     }
 
+public WeaponBase M16 = new
+
+
 }
+
